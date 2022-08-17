c467bee22d7978af04bdbdbe198cd2013f2f0524
Tyler Nijmeh <tylernij@gmail.com>
2022-08-17 10:23:48 -0700

    Add isolated client tests
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ test/src/client/client_test.dart
@@ -0,0 +1,94 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:sprocd/src/client/client.dart';
+import 'package:sprocd/src/model/encoded_transaction.dart';
+import 'package:sprocd/src/model/metadata_header.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Client', () {
+    final tempDir = Directory.systemTemp.createTempSync();
+    tearDownAll(() => tempDir.deleteSync(recursive: true));
+
+    // Just creates a simple file that gets deleted before every test.
+    final exampleBlackboxOutput = File(join(tempDir.path, 'test'));
+    final exampleScript = File(join(tempDir.path, 'process.sh'))
+      ..createSync()
+      ..writeAsStringSync('touch ${exampleBlackboxOutput.path}');
+    tearDown(() {
+      if (exampleBlackboxOutput.existsSync()) {
+        exampleBlackboxOutput.delete();
+      }
+    });
+
+    test('Server is dead', () async {
+      final client = Client(host: 'doesnotexist', port: 9999, command: 'echo');
+      expect(client.connect, throwsA(isA<SocketException>()));
+    });
+
+    test('Server has nothing to serve', () async {
+      final dummyServer =
+          await ServerSocket.bind(InternetAddress.loopbackIPv4, 9999);
+      final client = Client(
+        host: 'localhost',
+        port: 9999,
+        command: 'bash ${exampleScript.path}',
+      );
+      dummyServer.listen((client) => client.close());
+      await client.connect();
+      await dummyServer.close();
+
+      // Processing should not have occurred.
+      expect(exampleBlackboxOutput.existsSync(), false);
+    });
+
+    test('Server serves us some data', () async {
+      final dummyServer =
+          await ServerSocket.bind(InternetAddress.loopbackIPv4, 9999);
+      final client = Client(
+        host: 'localhost',
+        port: 9999,
+        command: 'bash ${exampleScript.path}',
+      );
+      dummyServer.listen((client) async {
+        final transaction = EncodedTransaction(
+          [1, 2, 3, 4, 5],
+          header: MetadataHeader(id: 0, initTime: DateTime.now()).toString(),
+        );
+        client.add(transaction.toBytes());
+        await client.flush();
+        await client.close();
+      });
+      await client.connect();
+      await dummyServer.close();
+
+      // Processing should have succeeded.
+      expect(exampleBlackboxOutput.existsSync(), true);
+    });
+
+    test('Server serves us some data but bad blackbox command', () async {
+      final dummyServer =
+          await ServerSocket.bind(InternetAddress.loopbackIPv4, 9999);
+      final client = Client(
+        host: 'localhost',
+        port: 9999,
+        command: 'exit 1; bash ${exampleScript.path}',
+      );
+      dummyServer.listen((client) async {
+        final transaction = EncodedTransaction(
+          [1, 2, 3, 4, 5],
+          header: MetadataHeader(id: 0, initTime: DateTime.now()).toString(),
+        );
+        client.add(transaction.toBytes());
+        await client.flush();
+        await client.close();
+      });
+      await client.connect();
+      await dummyServer.close();
+
+      // Processing should not have occurred.
+      expect(exampleBlackboxOutput.existsSync(), false);
+    });
+  });
+}
