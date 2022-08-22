9585afd64f4ef94cf96dc0c3b2fb732a4a31cb64
Tyler Nijmeh <tylernij@gmail.com>
2022-08-22 14:10:44 -0700

    Better stream usage in data encode test
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- test/src/utils/data_encode_test.dart
+++ test/src/utils/data_encode_test.dart
@@ -3,10 +3,10 @@ import 'package:test/test.dart';
 
 void main() {
   test('Data encode and decode match as streams', () async {
-    final data = [List.generate(128, (index) => index)];
-    final dataStream = Stream.fromIterable(data);
+    final data = List.generate(128, (index) => index);
+    final dataStream = Stream.value(data);
     final encodedData = encode(dataStream);
     final decodedData = decode(encodedData);
-    expect(await decodedData.single, data.single);
+    expect(await decodedData.single, data);
   });
 }
