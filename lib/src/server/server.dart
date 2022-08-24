27fb96140fc395762b6bcb484f263a6814113c51
Tyler Nijmeh <tylernij@gmail.com>
2022-08-24 10:06:23 -0700

    Keep track of an ID
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/src/server/server.dart
+++ lib/src/server/server.dart
@@ -59,7 +59,7 @@ class Server {
 
   /// If the client is not yet registered, perform a handshake and send some
   /// input data. Return the working file.
-  Future<File?> _serveInput(Socket client) async {
+  Future<File?> _serveInput(Socket client, int id) async {
     final clientId = _clientId(client);
 
     await inputQ.scan();
@@ -67,30 +67,27 @@ class Server {
 
     // If there is nothing to do, disconnect them.
     if (file == null) {
-      debug('server: nothing to serve, closing: $clientId');
+      debug('server[$id]: nothing to serve, closing: $clientId');
       await client.close();
     } else {
-      // Take an ID to use for this transaction, then increment it.
-      final transactionId = _transactionId++;
-
       info(
-        'server: sending transaction to client: \n'
-        '=====================================\n'
-        'CLIENT: $clientId\n'
-        'INIT-DATE: ${_initTime.toIso8601String()}\n'
-        'ID: $transactionId\n'
-        'FILE: ${file.path}\n'
-        '=====================================',
+        'server[$id]: sending transaction to client: \n'
+            '=====================================\n'
+            'CLIENT: $clientId\n'
+            'INIT-DATE: ${_initTime.toIso8601String()}\n'
+            'ID: $id\n'
+            'FILE: ${file.path}\n'
+            '=====================================',
       );
 
       try {
         await client.addStream(encode(file.openRead()));
         await client.flush();
         await client.close();
-        info('server: finished sending data to client: $clientId');
+        info('server[$id]: finished sending data to client: $clientId');
         return file;
       } catch (e) {
-        error('server: failed to send data to client: $clientId');
+        error('server[$id]: failed to send data to client: $clientId');
         stderr.writeln(e.toString());
       }
     }
@@ -100,42 +97,45 @@ class Server {
 
   /// Handle an incoming connection from a client.
   Future<void> _handleConnection(Socket client) async {
+    // Take an ID to use for this transaction, then increment it.
+    final id = _transactionId++;
+
     final timeStart = DateTime.now();
 
     final clientId = _clientId(client);
 
-    info('server: client connected: $clientId');
+    info('server[$id]: client connected: $clientId');
 
     // Serve input file to this client if one exists.
-    final workingFile = await _serveInput(client);
+    final workingFile = await _serveInput(client, id);
     if (workingFile == null) return;
 
     // Write out the output file to the disk.
-    info('server: received transaction from client: $clientId');
+    info('server[$id]: received transaction from client: $clientId');
     final tempDir = await Directory.systemTemp.createTemp();
     final outName = basename(workingFile.path).replaceFirst(
       InputQ.workingFileSuffix,
       '.out',
     );
     final tmpFile = File(join(tempDir.path, outName));
-    debug('server: writing out to ${tmpFile.path}');
+    debug('server[$id]: writing out to ${tmpFile.path}');
     await tmpFile.openWrite().addStream(decode(client));
 
     // Check if we got nothing back from the client.
     if (await tmpFile.length() == 0) {
-      error('server: received failure from client: $clientId');
+      error('server[$id]: received failure from client: $clientId');
       await tmpFile.delete();
 
-      info('server: adding failed input back to queue');
+      info('server[$id]: adding failed input back to queue');
       final unWorkingFile = inputQ.toUnWorkingFile(workingFile);
       await workingFile.rename(unWorkingFile.path);
     } else {
       final outFile = File(join(outputDir.path, outName));
-      debug('server: moving ${tmpFile.path} to ${outFile.path}');
+      debug('server[$id]: moving ${tmpFile.path} to ${outFile.path}');
       await tmpFile.copy(outFile.path);
       await tmpFile.delete();
 
-      debug('server: deleting original at ${workingFile.path}');
+      debug('server[$id]: deleting original at ${workingFile.path}');
       await workingFile.delete();
     }
 
@@ -145,6 +145,6 @@ class Server {
 
     final timeEnd = DateTime.now();
     final duration = timeEnd.difference(timeStart);
-    info('server: connection duration: $duration for client: $clientId');
+    info('server[$id]: connection duration: $duration for client: $clientId');
   }
 }
